80a67a85818725e4345fcf98da160b467feb94ab
Javier_GR <javigallego99@gmail.com>
2021-11-18 00:05:55 +0100

    Initial commit

---
+++ app/src/main/java/com/example/bookingcrossfitapp/AuthActivity.kt
@@ -0,0 +1,21 @@
+package com.example.bookingcrossfitapp
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class AuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+
+    }
+
+    private fun setup() {
+
+    }
+
+}

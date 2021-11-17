6ae8c119410ecf77d2ce9d1da934e1894dd32ad6
Javier_GR <javigallego99@gmail.com>
2021-11-18 00:36:47 +0100

    Sign up

--- app/src/main/java/com/example/bookingcrossfitapp/AuthActivity.kt
+++ app/src/main/java/com/example/bookingcrossfitapp/AuthActivity.kt
@@ -1,9 +1,13 @@
 package com.example.bookingcrossfitapp
 
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_auth.*
 
 class AuthActivity : AppCompatActivity() {
 
@@ -11,10 +15,37 @@ class AuthActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+        setup()
 
     }
 
     private fun setup() {
+        title = "Authentication"
+
+        signUpButton.setOnClickListener {
+            if(emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()) {
+                FirebaseAuth.getInstance()
+                    .createUserWithEmailAndPassword(
+                        (emailEditText.text.toString())
+                        ,passwordEditText.text.toString())
+                    .addOnCompleteListener {
+                        if(it.isSuccessful) {
+                            Toast.makeText(this, "Hi there! This is a Toast.", Toast.LENGTH_LONG).show()
+                        } else {
+                            showAlert()
+                        }
+                    }
+            }
+        }
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this);
+        builder.setTitle(("Error"))
+        builder.setMessage("Error while authentication")
+        builder.setPositiveButton("Accept", null);
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
 
     }
 

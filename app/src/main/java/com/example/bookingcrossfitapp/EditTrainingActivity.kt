8a65f921ede1f6fcaf50b0d9ba389c6206cd949a
Javier_GR <javigallego99@gmail.com>
2022-01-08 14:12:46 +0100

    Create and edit training xml design

---
+++ app/src/main/java/com/example/bookingcrossfitapp/EditTrainingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bookingcrossfitapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditTrainingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_training)
+    }
+}
